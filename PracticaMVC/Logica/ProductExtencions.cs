1fa0ce8dc5d018f70fcee1bf403e536407a596c1
Andres Aranda <andres.w.aranda@gmail.com>
2021-05-07 15:27:25 -0300

    Primer Push

---
+++ PracticaMVC/Logica/ProductExtencions.cs
@@ -0,0 +1,23 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logica
+{
+    public static class ProductExtencions
+    {
+        public static string Descriptor(this Product product)
+        {
+            string descripcion = $"Nombre:{product.ProductName}\n" +
+                $"Precio unitario:{product.UnitPrice}\n" +
+                $"Categoria:{product.Category.CategoryName}\n" +
+                $"Proveedor:{product.Supplier.CompanyName}\n" +
+                $"Stock actual:{product.UnitsInStock}\n" +
+                $"Unidade ordenadas;{product.UnitsOnOrder}\n";
+            return descripcion;
+        }
+    }
+}
---
+++ PracticaMVC/PresentacionMVC/Controllers/CategoryController.cs
@@ -0,0 +1,91 @@
+﻿using Entidades;
+using Logica;
+using PresentacionMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PresentacionMVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private CategoryLogic logic = new CategoryLogic();
+        // GET: Category
+        public ActionResult Index()
+        {
+            List<Category> lista = logic.GetAll();
+
+            List<CategoryView> listaCategorias = lista.Select(x => new CategoryView
+            {
+
+                ID = x.CategoryID,
+                Descripcion = x.Description,
+                Nombre = x.CategoryName,
+                Imagen = x.Picture
+
+            }).ToList();
+
+
+            return View(listaCategorias);
+        }
+        public ActionResult Delete(int id)
+        {
+            logic.Delete(id);
+            return RedirectToAction("Index");
+        }
+        public ActionResult NewEdit(int ID, bool Nuevo)
+        {
+            CategoryView view = new CategoryView();
+
+            if (!Nuevo)
+            {
+                CategoryLogic logic = new CategoryLogic();
+
+                Category entity = logic.GetOne((int)ID);
+
+                view = new CategoryView
+                {
+
+                    ID = entity.CategoryID,
+                    Descripcion = entity.Description,
+                    Nombre = entity.CategoryName,
+                    Imagen = entity.Picture
+
+                };
+            }
+
+            return View(view);
+        }
+        [HttpPost]
+        public ActionResult Insert(CategoryView categoryView)
+        {
+            try
+            {
+                Category categoryEntity = new Category
+                {
+                    CategoryName = categoryView.Nombre,
+                    Description = categoryView.Descripcion,
+                };
+
+                if (categoryView.ID == 0)
+                {
+                    logic.Insert(categoryEntity);
+                }
+                else
+                {
+                    categoryEntity.CategoryID = categoryView.ID;
+                    logic.Update(categoryEntity);
+                }
+
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("Index", "Error");
+            }
+        }
+    }
+}
